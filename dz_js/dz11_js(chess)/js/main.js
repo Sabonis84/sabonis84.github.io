07aee6a5e1756f65b0d92a81fd6014a11789e5cf
Aleksey <gorbatenkoal84@gmail.com>
2020-12-23 17:13:04 +0200

    var5

--- dz_js/dz11_js(chess)/js/main.js
+++ dz_js/dz11_js(chess)/js/main.js
@@ -7,29 +7,30 @@ for (let i = 0; i < 8; i++) {
   for (let j = 0; j < 8; j++) {
     cont = cont + 1;
      if(i % 2 !==0 ) {
-        str += j % 2 === 0 ? `<div class="cub white" id="${cont}"></div>`: `<div class="cub black"id="${cont}"></div>`
-      } else {
         str += j % 2 === 0 ? `<div class="cub black" id="${cont}"></div>`: `<div class="cub white"id="${cont}"></div>`
+      } else {
+        str += j % 2 === 0 ? `<div class="cub white" id="${cont}"></div>`: `<div class="cub black"id="${cont}"></div>`
       }
      }
    }  
     
   mainEl.innerHTML = str
   for (let i = 7; i <= 15; i++) {
-    mainEl.children[i].innerHTML = `<div class="peshkaW">&#9817</div>`
+    mainEl.children[i].innerHTML = `<div class="peshkaW">&#9823;</div>`
     }
-  mainEl.children[0].innerHTML = `<div class="ladyaW">&#9814;</div>`;
-  mainEl.children[7].innerHTML = `<div class="ladyaW">&#9814;</div>`;
-  mainEl.children[1].innerHTML = `<div class="horseW">&#9816;</div>`;
-  mainEl.children[6].innerHTML = `<div class="horseW">&#9816;</div>`;
-  mainEl.children[2].innerHTML = `<div class="oficerW">&#9815;</div>`;
-  mainEl.children[5].innerHTML = `<div class="oficerW">&#9815;</div>`;
-  mainEl.children[3].innerHTML = `<div class="queenW">&#9813;</div>`;
-  mainEl.children[4].innerHTML = `<div class="kingW">&#9812;</div>`;
-
   for (let i = 48; i < 56; i++) {
-    mainEl.children[i].innerHTML = `<div class="peshkaB">&#9823;</div>`
-  }
+      mainEl.children[i].innerHTML = `<div class="peshkaB">&#9823;</div>`
+    }
+  mainEl.children[0].innerHTML = `<div class="ladyaW">&#9820;</div>`;
+  mainEl.children[7].innerHTML = `<div class="ladyaW">&#9820;</div>`;
+  mainEl.children[1].innerHTML = `<div class="horseW">&#9822;</div>`;
+  mainEl.children[6].innerHTML = `<div class="horseW">&#9822;</div>`;
+  mainEl.children[2].innerHTML = `<div class="oficerW">&#9821;</div>`;
+  mainEl.children[5].innerHTML = `<div class="oficerW">&#9821;</div>`;
+  mainEl.children[3].innerHTML = `<div class="queenW">&#9819;</div>`;
+  mainEl.children[4].innerHTML = `<div class="kingW">&#9818;</div>`;
+
+  
   mainEl.children[56].innerHTML = `<div class="ladyaB">&#9820;</div>`;
   mainEl.children[63].innerHTML = `<div class="ladyaB">&#9820;</div>`;
   mainEl.children[57].innerHTML = `<div class="horseB">&#9822;</div>`;

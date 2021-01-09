608530828a22838280b92434e06aa7c7f62de809
Aleksey <gorbatenkoal84@gmail.com>
2021-01-09 16:49:49 +0200

    var2

--- 04-01-2021/js/main.js
+++ 04-01-2021/js/main.js
@@ -1,5 +1,11 @@
 const mainEl = document.querySelector('.main')
-let time = moment()
-let currTime = time.format('DD-MM-YYYY HH:mm:ss')
-console.log(currTime);
-mainEl.innerHTML = `<div class="current-time">${currTime}</div>`
+let id;
+const runTime = () => {
+    id = setInterval( () => {
+        let time = moment()
+        let currTime = time.format('DD-MM-YYYY HH:mm:ss')
+        console.log(currTime);
+        mainEl.innerHTML = `<div class="current-time">${currTime}</div>`
+    }, 1000);
+};
+runTime();
